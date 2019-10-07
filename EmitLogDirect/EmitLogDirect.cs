044ec165c2041fbf9418d2fefddbdbc36fabca5f
Ethan Christie <ethan.j.christie@outlook.com>
2019-10-07 17:44:56 +1000

    Add part 4 - routing

---
+++ EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+
+namespace EmitLogDirect
+{
+    public class EmitLogDirect
+    {
+        private const string ExchangeName = "direct_logs";
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var basicProperties = new BasicProperties();
+                basicProperties.Timestamp = new AmqpTimestamp(DateTime.Now.Ticks);
+
+                var severity = args.Length > 0 ? args[0] : "info";
+
+                channel.BasicPublish(exchange: ExchangeName,
+                    routingKey: severity,
+                    basicProperties: basicProperties,
+                    body: body
+                );
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return args.Length < 1 ? "Hello World!" : string.Join(" ", args.Skip(1).ToArray());
+        }
+    }
+}
---
+++ ReceiveLogDirect/ReceiveLogDirect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogDirect
+{
+    public class ReceiveLogDirect
+    {
+        private const string ExchangeName = "direct_logs";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]", Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct);
+                var queueName = channel.QueueDeclare().QueueName;
+
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: ExchangeName,
+                        routingKey: severity
+                    );
+                }
+
+                Console.WriteLine(" [*] Waiting for messages");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var timeStamp = new DateTime(ea.BasicProperties.Timestamp.UnixTime);
+
+                    Console.WriteLine($"[{timeStamp}] [{ea.RoutingKey}] Received");
+                    Console.WriteLine($"    {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
