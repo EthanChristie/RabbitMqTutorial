fb61ed710d7d7d15df7f7ab771415d5f1261b132
Ethan Christie <ethan.j.christie@outlook.com>
2019-10-08 10:27:10 +1000

    Add part 5 - Topics

---
+++ EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+
+namespace EmitLogTopic
+{
+    public class EmitLogTopic
+    {
+        private const string ExchangeName = "topic_logs";
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic);
+
+                var message = GetMessage(args);
+                var routingKey = args.Length > 0 ? args[0] : "anonymous.info";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var basicProperties = new BasicProperties
+                {
+                    Timestamp = new AmqpTimestamp(DateTime.Now.Ticks)
+                };
+
+
+                channel.BasicPublish(exchange: ExchangeName,
+                    routingKey: routingKey,
+                    basicProperties: basicProperties,
+                    body: body
+                );
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return args.Length < 1 ? "Hello World!" : string.Join(" ", args.Skip(1).ToArray());
+        }
+    }
+}
---
+++ ReceiveLogTopic/ReceiveLogTopic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogTopic
+{
+    public class ReceiveLogTopic
+    {
+        private const string ExchangeName = "topic_logs";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: {0} [binding_key...]", Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic);
+                var queueName = channel.QueueDeclare().QueueName;
+
+
+                foreach (var bindingKey in args)
+                {
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: ExchangeName,
+                        routingKey: bindingKey
+                    );
+                }
+
+                Console.WriteLine(" [*] Waiting for messages");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var timeStamp = new DateTime(ea.BasicProperties.Timestamp.UnixTime);
+
+                    Console.WriteLine($"[{timeStamp}] [{ea.RoutingKey}] Received");
+                    Console.WriteLine($"    {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
